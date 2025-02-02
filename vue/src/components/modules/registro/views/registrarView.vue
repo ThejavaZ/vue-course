0e72588020dda0c7a10014be8679d4d7b0f05677
JavierSG <jsdash10000@gmail.com>
2025-02-02 15:41:27 -0700

    Se agrego un formulario y se dividio en modulos los componentes de contador, la lista y se creo el registro de usuarios, ademas de las validaciones del form

---
+++ vue/src/components/modules/registro/views/registrarView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>Formulario de registro</h2>
+    <Form :validationSchema="schema" @submit="onSubmit">
+      <div class="form">
+        <label for="">Nombre:</label>
+        <Field
+          type="text"
+          name="nombre"
+          id=""
+          placeholder="ingrese su nombre"
+        />
+        <ErrorMessage name="nombre" />
+      </div>
+      <br />
+      <div class="form">
+        <label for="email">Email:</label>
+        <Field type="email" name="email" id="" placeholder="ingrese su email" />
+        <ErrorMessage name="email" />
+      </div>
+      <br />
+      <div class="form">
+        <button type="submit">registar</button>
+      </div>
+    </Form>
+  </div>
+</template>
+
+<script setup>
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { schema } from "../schemas/validationSchema";
+
+const onSubmit = () => {
+  console.log("se ha enviado el formulario");
+};
+</script>
+
+<style scoped>
+.form {
+  margin-bottom: 20px;
+}
+</style>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Contador from "../components/Contador.vue";
-import ListaDeTareas from "@/components/ListaDeTareas.vue";
+import Contador from "../components/modules/contador/components/Contador.vue";
+import ListaDeTareas from "../components/modules/lista de tareas/components/ListaDeTareas.vue";
+import registrarView from "../components/modules/registro/views/registrarView.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -18,6 +20,11 @@ const router = createRouter({
       path: "/lista-de-tareas",
       name: "lista-de-tareas",
       component: ListaDeTareas,
+    },
+    {
+      path: "/registrar",
+      name: "registrar",
+      component: registrarView,
     },
   ],
 });
